9b3b4846f0c5514ec626c305ebb5f05fa4427384
BenHbrt <benherbert1983@googlemail.com>
2023-01-31 12:30:21 +0100

    Function for loading currencies list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.scss';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  // State for holding list of currencies
+  const [currencies, setCurrencies] = useState(null)
+
+  // Function for loading list of currencies
+  const loadCurrencies = async () => {
+    const response = await axios.get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json")
+    const currenciesList = []
+    Object.keys(response.data).forEach((item) => {
+      const currencyObj = {}
+      currencyObj.code = item
+      currencyObj.name = response.data[item]
+      currenciesList.push(currencyObj)
+    })
+    setCurrencies(currenciesList)
+  }
+
+  // Load list of currencies after page has loaded
+  useEffect(() => {
+    loadCurrencies()
+  }, [])
+
   return (
     <div className="App">
       <h1>Text</h1>
+      <button onClick={() => console.log(currencies)}>Currencies List</button>
     </div>
   );
 }
